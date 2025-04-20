50d0c938e080ecee4c58e5a60c40179bea2b6935
SAS_OP <14040386+sasop3@users.noreply.github.com>
2025-04-20 17:17:23 +0300

    Made image finding consistency much better, Open-CV also is used

--- app.py
+++ app.py
@@ -4,20 +4,23 @@ ScreenX,ScreenY = pyautogui.size()
 
 
 
+loopcount = 25
 pyautogui.FAILSAFE = True
 mouseX,mouseY = pyautogui.position()
-for x in range(30):
-    firefoxpos = pyautogui.locateOnScreen("firefox.png")
+
+
+for x in range(loopcount):
+    firefoxpos = pyautogui.locateOnScreen("firefox.png",confidence=0.9)
     firefoxpos = pyautogui.center(firefoxpos)
     pyautogui.leftClick(firefoxpos)
 
     pyautogui.leftClick(138,23) #click first tab on firefox
-    snippos = pyautogui.locateOnScreen("snippingtool.png")
+    snippos = pyautogui.locateOnScreen("snippingtool.png",confidence=0.9)
     snippos = pyautogui.center(snippos)
     pyautogui.leftClick(snippos)
     pyautogui.sleep(0.5)
 
-    newbuttonpos = pyautogui.locateOnScreen("newbutton.png")
+    newbuttonpos = pyautogui.locateOnScreen("newbutton.png",confidence=0.99)
     newbuttonpos = pyautogui.center(newbuttonpos)
     pyautogui.leftClick(newbuttonpos)
 
@@ -27,26 +30,34 @@ for x in range(30):
 
 
     pyautogui.sleep(0.7) 
-    extractbuttonpos = pyautogui.locateOnScreen("extractbutton.png")
+    try:
+        extractbuttonpos = pyautogui.locateOnScreen("extractbutton.png",confidence=0.8)
+    except pyautogui.ImageNotFoundException:
+        try:
+            extractbuttonpos = pyautogui.locateOnScreen("extractbutton2.png",confidence=0.7)
+        except pyautogui.ImageNotFoundException:
+            extractbuttonpos = pyautogui.locateOnScreen("extractbutton3.png",confidence=0.7)
+
+    
     extractbuttonpos = pyautogui.center(extractbuttonpos)
     pyautogui.leftClick(extractbuttonpos)
     pyautogui.sleep(2) 
 
 
+    copybutton = pyautogui.locateOnScreen("copyallbutton2.png",confidence=0.9)
 
-    copybutton = pyautogui.locateOnScreen("copyallbutton.png")
     copybutton = pyautogui.center(copybutton)
     pyautogui.leftClick(copybutton)
     pyautogui.sleep(0.7)
 
 
-    firefox2pos = pyautogui.locateOnScreen("firefox2.png")
-    firefox2pos = pyautogui.center(firefox2pos)
-    pyautogui.leftClick(firefox2pos)
+    pyautogui.leftClick(firefoxpos)
+    pyautogui.sleep(0.25)
     pyautogui.press('right')
 
 
-    notepad = pyautogui.locateOnScreen("notepad.png")
+
+    notepad = pyautogui.locateOnScreen("notepad.png",confidence=0.9)
     notepad = pyautogui.center(notepad)
     pyautogui.leftClick(notepad)
 
